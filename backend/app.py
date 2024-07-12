8a396d5497b015a3130f14f6487afccdb2adefe6
Ed Chapman <edchapman88@gmail.com>
2024-07-12 19:59:14 +0100

    Chore: Allow all origins for development purposes.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from services.fetch_data import get_messages, get_report
 from services.calculate_credits import calculate_credits
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/usage")
